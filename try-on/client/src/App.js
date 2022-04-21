957f95f4016119ed4062defb324ff1f7563351a7
bilal <me@example.com>
2022-04-21 15:04:45 +0500

    tryon client setup

---
+++ try-on/client/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="bg-indigo-500">
+        Hello world this is first page of the website
+      </div>
+    </>
+  );
+}
+
+export default App;
