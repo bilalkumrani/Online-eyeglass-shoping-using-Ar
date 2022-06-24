9e604775f60ee042ac61a4cbd9c1f93be951ff29
noumanali0 <alinouman278@gmail.com>
2022-06-25 01:36:13 +0500

    corsIssue

--- try-on/frontend/src/components/Allproducts.js
+++ try-on/frontend/src/components/Allproducts.js
@@ -4,20 +4,20 @@ import axios from "axios";
 
 const Allproducts = () => {
   useEffect(() => {
-    axios.get("https://localhost:4000");
+    axios
+      .get("https://localhost:4000/product/all", {
+        header: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
+        },
+      })
+      .then((res) => console.log(res.json()));
   }, []);
 
   return (
     <div className="container" style={{ marginTop: "100px" }}>
       <div className="row">
-        <Framescards />
-        <Framescards />
-        <Framescards />
-        <Framescards />
-        <Framescards />
-        <Framescards />
-        <Framescards />
-        <Framescards />
+        <h2>All Products Here</h2>
       </div>
     </div>
   );
