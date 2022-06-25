5acfe264a08b4c4a628c52edcf59e16943763efc
noumanali0 <alinouman278@gmail.com>
2022-06-25 12:08:37 +0500

    cartAndOtherCodeBlocksAdded

--- try-on/frontend/src/components/CartBtn.js
+++ try-on/frontend/src/components/CartBtn.js
@@ -3,16 +3,33 @@ import Badge from "@mui/material/Badge";
 import { AiOutlineShoppingCart } from "react-icons/ai";
 
 export default function CartBtn() {
-  return (
-    <Badge
-      variant="dot"
-      badgeContent=" "
-      sx={{
-        m: 2,
-      }}
-      color="secondary"
-    >
-      <AiOutlineShoppingCart size={25} />
-    </Badge>
-  );
+  const length = 1;
+
+  {
+    if (length === 0) {
+      return (
+        <Badge
+          sx={{
+            m: 2,
+          }}
+          color="secondary"
+        >
+          <AiOutlineShoppingCart size={25} />
+        </Badge>
+      );
+    } else {
+      return (
+        <Badge
+          variant="dot"
+          badgeContent=" "
+          sx={{
+            m: 2,
+          }}
+          color="secondary"
+        >
+          <AiOutlineShoppingCart size={25} />
+        </Badge>
+      );
+    }
+  }
 }
--- try-on/frontend/src/components/navigation.jsx
+++ try-on/frontend/src/components/navigation.jsx
@@ -1,8 +1,28 @@
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { FaSearch } from "react-icons/fa";
 import CartBtn from "./CartBtn";
+import Button from "@mui/material/Button";
+import Backdrop from "@mui/material/Backdrop";
+import Box from "@mui/material/Box";
+import Modal from "@mui/material/Modal";
+import Fade from "@mui/material/Fade";
+import Typography from "@mui/material/Typography";
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
 
+  boxShadow: 24,
+  p: 4,
+};
 export const Navigation = () => {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
   return (
     <nav id="menu" className="navbar navbar-default navbar-fixed-top">
       <div className="container">
@@ -39,6 +59,7 @@ export const Navigation = () => {
                 Eyeglasses
               </Link>
             </li>
+
             <div
               style={{ display: "inline" }}
               onClick={() => {
@@ -47,6 +68,52 @@ export const Navigation = () => {
             >
               <CartBtn />
             </div>
+            <li>
+              <Button
+                style={{ marginTop: "10px", color: "gray" }}
+                onClick={handleOpen}
+              >
+                Sign in
+              </Button>
+              <Modal
+                aria-labelledby="transition-modal-title"
+                aria-describedby="transition-modal-description"
+                open={open}
+                onClose={handleClose}
+                closeAfterTransition
+                BackdropComponent={Backdrop}
+                BackdropProps={{
+                  timeout: 500,
+                }}
+              >
+                <Fade in={open}>
+                  <Box sx={style}>
+                    <Typography
+                      id="transition-modal-title"
+                      variant="h6"
+                      component="h2"
+                    >
+                      Text in a modal
+                    </Typography>
+                    <Typography
+                      id="transition-modal-description"
+                      sx={{ mt: 2 }}
+                    >
+                      Duis mollis, est non commodo luctus, nisi erat porttitor
+                      ligula.
+                    </Typography>
+                  </Box>
+                </Fade>
+              </Modal>
+            </li>
+            <li>
+              <Button
+                style={{ marginTop: "10px", color: "gray" }}
+                onClick={handleClose}
+              >
+                Sign up
+              </Button>
+            </li>
           </ul>
 
           <form action="" className="search-form">
