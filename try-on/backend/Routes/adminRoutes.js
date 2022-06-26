f3ab431d3f8c1d74735bac67ce28c22c81cd5e71
bilal <b.umrani75@gmail.com>
2022-06-26 14:08:21 +0500

    [add] edit product

--- try-on/backend/Routes/adminRoutes.js
+++ try-on/backend/Routes/adminRoutes.js
@@ -2,10 +2,12 @@ const express = require("express");
 const addProduct = require("../Controllers/adminControllers/addProductController");
 const removeProduct = require("../Controllers/adminControllers/removeProductController");
 const login = require("../Controllers/adminControllers/loginController");
+const edit = require("../Controllers/adminControllers/editProductController");
 const isAdmin = require("../Middleware/isAdmin");
 const router = express.Router();
 
 router.get("/login", login);
 router.post("/add", isAdmin, addProduct);
 router.delete("/remove", isAdmin, removeProduct);
+router.patch("/edit", isAdmin, edit);
 module.exports = router;
