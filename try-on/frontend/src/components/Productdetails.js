490bc59d2db3d176357970cfd3350e43c1e8958f
bilal <b.umrani75@gmail.com>
2022-09-11 14:04:27 +0500

    lil changes

--- try-on/frontend/src/components/Productdetails.js
+++ try-on/frontend/src/components/Productdetails.js
@@ -7,6 +7,7 @@ import { useDispatch } from "react-redux";
 import { addItem, deleteItem } from "../redux/actions/index";
 import { useParams, useNavigate } from "react-router-dom";
 import Button from "@mui/material/Button";
+import axios from "axios";
 
 export default function Productdetails() {
   const dispatch = useDispatch();
@@ -16,9 +17,85 @@ export default function Productdetails() {
   const { id } = useParams();
   let navigate = useNavigate();
 
-  const payment = () => {
-    navigate("/payment");
-  };
+  // razor
+  // const payment = () => {
+  //   navigate("/payment");
+  // };
+
+  function loadScript(src) {
+    return new Promise((resolve) => {
+      const script = document.createElement("script");
+      script.src = src;
+      script.onload = () => {
+        resolve(true);
+      };
+      script.onerror = () => {
+        resolve(false);
+      };
+      document.body.appendChild(script);
+    });
+  }
+
+  async function displayRazorpay() {
+    const res = await loadScript(
+      "https://checkout.razorpay.com/v1/checkout.js"
+    );
+
+    if (!res) {
+      alert("Razorpay SDK failed to load. Are you online?");
+      return;
+    }
+
+    // creating a new order
+    const result = await axios.post("http://localhost:4000/payment/orders");
+
+    if (!result) {
+      alert("Server error. Are you online?");
+      return;
+    }
+
+    // Getting the order details back
+    const { amount, id: order_id, currency } = result.data;
+
+    const options = {
+      key: "rzp_test_TcGg5jnbUxtHCK", // Enter the Key ID generated from the Dashboard
+      amount: amount.toString(),
+      currency: currency,
+      name: "Bilal Khan.",
+      description: "Test Transaction",
+      Image: "",
+      order_id: order_id,
+      handler: async function (response) {
+        const data = {
+          orderCreationId: order_id,
+          razorpayPaymentId: response.razorpay_payment_id,
+          razorpayOrderId: response.razorpay_order_id,
+          razorpaySignature: response.razorpay_signature,
+        };
+
+        const result = await axios.post(
+          "http://localhost:5000/payment/success",
+          data
+        );
+
+        alert(result.data.msg);
+      },
+      prefill: {
+        name: "Bilal",
+        email: "Bilal@example.com",
+        contact: "9999999999",
+      },
+      notes: {
+        address: "FrameBuy office",
+      },
+      theme: {
+        color: "#61dafb",
+      },
+    };
+
+    const paymentObject = new window.Razorpay(options);
+    paymentObject.open();
+  }
 
   return (
     <div className="card-wrapper">
@@ -98,7 +175,7 @@ export default function Productdetails() {
                 color: "black",
                 borderColor: "grey.500",
               }}
-              onClick={payment}
+              onClick={displayRazorpay}
             >
               Buy
             </Button>
