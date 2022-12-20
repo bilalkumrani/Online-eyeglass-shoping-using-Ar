a6dd30451fe3e40a88c62815fc2fcdb65c028b23
Bilal Khan <50484447+bilalscripts@users.noreply.github.com>
2022-12-20 22:43:11 +0500

    Update addProductController.js

--- try-on/backend/Controllers/adminControllers/addProductController.js
+++ try-on/backend/Controllers/adminControllers/addProductController.js
@@ -2,12 +2,15 @@ const Product = require("../../Models/Product");
 
 const addProduct = async (req, res) => {
   if (req.body) {
-    const { name, price, color } = req.body;
+    const { name, price, color, sku, tags, pic } = req.body;
     try {
       const product = await Product.create({
         name,
         price,
         color,
+        tags,
+        pic,
+        sku
       });
       return res.json({ status: "ok", product });
     } catch (error) {
